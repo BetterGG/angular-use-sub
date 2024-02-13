4d07564a0b9a026e20b82a9d7fd098848c66514e
gongguan <1285462534@qq.com>
2024-02-13 21:26:12 +0800

    feat: 回到初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonTitleModule } from '../subCommon/common-title/common-title.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { CommonTitleModule } from '../subCommon/common-title/common-title.module
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonTitleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
